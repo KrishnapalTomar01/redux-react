9a4633ee552b04bc4c1e6ba8e8b55c90a64a575e
Krishnapal Tomar <krishnapal.tomar@thepsi.com>
2025-02-27 17:58:19 +0530

    Using redux toolkit and redux devtools

---
+++ src/store-v1.js
@@ -0,0 +1,30 @@
+import { applyMiddleware, combineReducers, createStore } from "redux";
+import { thunk } from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+import accountReducer from "./features/accounts/accountSlice";
+import customerReducer from "./features/customers/customerSlice";
+
+const rootReducer = combineReducers({
+  account: accountReducer,
+  customer: customerReducer,
+});
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
+
+export default store;
+
+// store.dispatch({ type: "account/deposit", payload: 500 });
+
+// console.log(store.getState());
+
+// store.dispatch(deposit(500));
+
+//console.log(store.getState());
+
+// store.dispatch(createCustomer("Krish reg", "2423sf"));
+
+// console.log(store.getState());
--- src/store.js
+++ src/store.js
@@ -1,26 +1,13 @@
-import { applyMiddleware, combineReducers, createStore } from "redux";
-import { thunk } from "redux-thunk";
+import { configureStore } from "@reduxjs/toolkit";
 
 import accountReducer from "./features/accounts/accountSlice";
 import customerReducer from "./features/customers/customerSlice";
 
-const rootReducer = combineReducers({
-  account: accountReducer,
-  customer: customerReducer,
+const store = configureStore({
+  reducer: {
+    account: accountReducer,
+    customer: customerReducer,
+  },
 });
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
-
 export default store;
-
-// store.dispatch({ type: "account/deposit", payload: 500 });
-
-// console.log(store.getState());
-
-// store.dispatch(deposit(500));
-
-//console.log(store.getState());
-
-// store.dispatch(createCustomer("Krish reg", "2423sf"));
-
-// console.log(store.getState());
